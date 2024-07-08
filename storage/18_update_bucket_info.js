8b460612ad98773b5c474e8985f4001925f604a3
phenix3443 <phenix3443@gmail.com>
2024-07-08 16:37:05 +0800

    feat: add updateBucketInfo test script

---
+++ storage/18_update_bucket_info.js
@@ -0,0 +1,36 @@
+import { ethers } from "ethers";
+import fs from "fs-extra";
+import path from "path";
+
+export const main = async () => {
+  try {
+    const { rpc, contracts, storageAddress, privateKey } = await fs.readJSON(
+      "../cfg.json"
+    );
+    const { abi } = await fs.readJSON(
+      path.join(contracts, "storage/IStorage.sol/IStorage.json")
+    );
+    const provider = new ethers.JsonRpcProvider(rpc);
+
+    // input params
+    const wallet = new ethers.Wallet(privateKey, provider);
+    const bucketName = "mechain";
+    const paymentAddress = wallet.address;
+    const visibility = 2;
+    const chargedReadQuota = "100000000000000";
+
+    const storage = new ethers.Contract(storageAddress, abi, wallet);
+    const tx = await storage.updateBucketInfo(
+      bucketName,
+      visibility,
+      paymentAddress,
+      chargedReadQuota
+    );
+    const receipt = await tx.wait();
+    console.log("delete bucket success, receipt: ", receipt);
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+main();
