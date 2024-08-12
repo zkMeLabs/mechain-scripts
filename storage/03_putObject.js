be0eb14e0ef8e84eaccf3b46a888f5c43a462ad8
Athas <wanghao@zkme.xyz>
2024-08-12 10:26:30 +0800

    fix: upload file for devnet

--- storage/03_putObject.js
+++ storage/03_putObject.js
@@ -6,30 +6,37 @@ import { utf8ToBytes } from 'ethereum-cryptography/utils.js';
 
 export const main = async () => {
   try {
-    const filePath = './go.sum';
+    const filePath = './uploadObject/temp.txt';
     const fileBuffer = fs.readFileSync(filePath);
-    const { privateKey } = await fs.readJSON('../cfg.json');
+    const { bucketName, objectName, privateKey } =
+      await fs.readJSON('../cfg.json');
 
     // input params
     const payloadSize = fileBuffer.length;
     const contentType = 'application/octet-stream';
     const currentDate = new Date().toISOString();
     const expiryTimestamp = new Date(Date.now() + 1000 * 1000).toISOString();
+    const sphost = '154.48.244.45:9036'; //sphost ip:port
     const canonicalRequest = [
       'PUT',
-      '/mechain/go.sum',
+      '/' + bucketName + '/' + objectName,
       '',
-      'content-type:application/octet-stream',
+      'content-type:' + contentType,
       'x-gnfd-content-sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
       'x-gnfd-date:' + currentDate,
       'x-gnfd-expiry-timestamp:' + expiryTimestamp,
-      '192.168.0.99:9033',
+      sphost,
       '',
       'content-type;x-gnfd-content-sha256;x-gnfd-date;x-gnfd-expiry-timestamp',
     ].join('\n');
+    console.log(
+      `bucketName = ${bucketName}, objectName = ${objectName}, data = ${fileBuffer.toString()}`,
+    );
+    // console.log(canonicalRequest);
+
     const canonicalRequestBytes = utf8ToBytes(canonicalRequest);
     const unsignedMsg = keccak256(canonicalRequestBytes);
-    const signingKey = new SigningKey(privateKey);
+    const signingKey = new SigningKey('0x' + privateKey);
     const signature = signingKey.signDigest(unsignedMsg);
     let sig = joinSignature(signature);
     const v = sig.slice(-2);
@@ -38,26 +45,25 @@ export const main = async () => {
     const authorization = `GNFD1-ECDSA, Signature=${sig.slice(2)}`;
 
     const options = {
-      url: 'http://192.168.0.99:9033/mechain/go.sum',
+      url: 'http://' + sphost + '/' + bucketName + '/' + objectName,
       method: 'PUT',
-      path: '/mechain/go.sum',
+      path: '/' + bucketName + '/' + objectName,
       httpVersion: 1.1,
       headers: {
-        HOST: '192.168.0.99:9033',
+        'HOST': sphost,
         'User-Agent': 'Greenfield (linux; amd64) greenfield-go-sdk/v0.1.0',
         'Content-Length': payloadSize,
-        Authorization: authorization,
+        'Authorization': authorization,
         'Content-Type': contentType,
-        'X-Gnfd-Content-Sha256':
-          'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
+        'X-Gnfd-Content-Sha256': 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
         'X-Gnfd-Date': currentDate,
         'X-Gnfd-Expiry-Timestamp': expiryTimestamp,
-        'Accept-Encoding': 'gzip',
+        'Accept-Encoding': 'gzip'
       },
-      body: fileBuffer,
+      body: fileBuffer
     };
 
-    fetch('http://192.168.0.99:9033/mechain/go.sum', options)
+    fetch('http://' + sphost + '/' + bucketName + '/' + objectName, options)
       .then((response) => {
         if (response.ok) {
           console.log('File data sent successfully!');
