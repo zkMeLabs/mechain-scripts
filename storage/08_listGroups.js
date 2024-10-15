921d42b6b0cb859269fcec2db720312a6801e6e9
phenix3443 <phenix3443@gmail.com>
2024-10-15 15:58:54 +0800

    feat: tune code

--- storage/08_listGroups.js
+++ storage/08_listGroups.js
@@ -3,10 +3,8 @@ import fs from 'fs-extra';
 import path from 'path';
 
 export const main = async () => {
-  const { rpc, contracts, storageAddress } = await fs.readJSON('../cfg.json');
-  const { abi } = await fs.readJSON(
-    path.join(contracts, 'storage/IStorage.sol/IStorage.json'),
-  );
+  const { rpc, contracts, storageAddress, privateKey } = await fs.readJSON('../cfg.json');
+  const { abi } = await fs.readJSON(path.join(contracts, 'storage/IStorage.sol/IStorage.json'));
   const provider = new ethers.JsonRpcProvider(rpc);
 
   // input params
@@ -17,13 +15,11 @@ export const main = async () => {
     countTotal: false,
     reverse: false,
   };
-  const groupOwner = '0x62d574476d10f5745DC0c686c280762f97251c8a';
+  const wallet = new ethers.Wallet(privateKey);
+  const groupOwner = wallet.address;
 
   const storage = new ethers.Contract(storageAddress, abi, provider);
-  const [groups, pageResponse] = await storage.listGroups(
-    pageRequest,
-    groupOwner,
-  );
+  const [groups, pageResponse] = await storage.listGroups(pageRequest, groupOwner);
   // console.log('groups', groups.toObject(true));
   for (const group of groups) {
     console.log('group', group.toObject(true));
--- storage/13_headGroupMember.js
+++ storage/13_headGroupMember.js
@@ -3,23 +3,15 @@ import fs from 'fs-extra';
 import path from 'path';
 
 export const main = async () => {
-  const { rpc, contracts, storageAddress, privateKey } =
-    await fs.readJSON('../cfg.json');
-  const { abi } = await fs.readJSON(
-    path.join(contracts, 'storage/IStorage.sol/IStorage.json'),
-  );
+  const { rpc, contracts, storageAddress, privateKey, groupName } = await fs.readJSON('../cfg.json');
+  const { abi } = await fs.readJSON(path.join(contracts, 'storage/IStorage.sol/IStorage.json'));
   const provider = new ethers.JsonRpcProvider(rpc);
   const wallet = new ethers.Wallet(privateKey);
   const member = '0x757F0F85A5184c4E0183525EbD8fEdcD53b7eF80';
   const groupOwner = wallet.address;
-  const groupName = 'mechain';
 
   const storage = new ethers.Contract(storageAddress, abi, provider);
-  const groupinfo = await storage.headGroupMember(
-    member,
-    groupOwner,
-    groupName,
-  );
+  const groupinfo = await storage.headGroupMember(member, groupOwner, groupName);
   console.log('groupmember:', groupinfo.toObject(true));
 };
 
