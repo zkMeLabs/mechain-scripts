86280d55bdbcdae54bff2684c1dc565356226993
Luke <luchenqun@qq.com>
2024-10-24 09:35:39 +0800

    feat(precompile): update precompile contract for gov module

--- gov/04_deposit.js
+++ gov/04_deposit.js
@@ -12,12 +12,12 @@ export const main = async () => {
   const provider = new ethers.JsonRpcProvider(rpc);
 
   // input params
-  const privateKey = 'e54bff83fc945cba77ca3e45d69adc5b57ad8db6073736c8422692abecfb5fe2';
+  const privateKey = 'f78a036930ce63791ea6ea20072986d8c3f16a6811f6a2583b0787c45086f769';
   const wallet = new ethers.Wallet(privateKey, provider);
   const gov = new ethers.Contract(govAddress, abi, wallet);
 
   const proposalId = 1;
-  const amount = ethers.Typed.uint256('9000000');
+  const amount = ethers.Typed.uint256('1000000000000000000');
   const tx = await gov.deposit(proposalId, amount);
   const receipt = await tx.wait();
   console.log(`deposit proposal success, blockNumber: ${receipt.blockNumber}, blockHash: ${receipt.blockHash}`);
--- gov/15_test.js
+++ gov/15_test.js
@@ -23,19 +23,11 @@ export const main = async () => {
   {
     const messages = JSON.stringify([
       {
-        '@type': '/cosmos.gov.v1.MsgExecLegacyContent',
+        '@type': '/cosmos.bank.v1beta1.MsgUpdateParams',
         authority: '0x7b5Fe22B5446f7C62Ea27B8BD71CeF94e03f3dF2',
-        content: {
-          '@type': '/cosmos.params.v1beta1.ParameterChangeProposal',
-          title: 'Bank Module Parameter Change: DefaultSendEnabled.',
-          description: 'change default_send_enabled from true to false',
-          changes: [
-            {
-              subspace: 'bank',
-              key: 'DefaultSendEnabled',
-              value: 'false',
-            },
-          ],
+        params: {
+          send_enabled: [],
+          default_send_enabled: false,
         },
       },
     ]);
@@ -45,13 +37,13 @@ export const main = async () => {
     const initialDeposit = [
       {
         denom,
-        amount: '10000000',
+        amount: '1000000000000000000',
       },
     ];
 
     const tx = await gov.submitProposal(messages, initialDeposit, metadata, title, summary);
     const receipt = await tx.wait();
-    console.log('legacy submit proposal success, receipt: ', receipt);
+    console.log('submit proposal success, receipt: ', receipt);
   }
 
   console.log('wait 6s to vote the proposal');
