30f0c453ec618235ef25e58ab681d2add84087dd
kevin-rd <kevin@zkme.xyz>
2024-11-18 15:06:02 +0800

    feat: putObject auto obtain sp endpoint

--- storage/03_putObject.js
+++ storage/03_putObject.js
@@ -8,48 +8,39 @@ import { utf8ToBytes } from 'ethereum-cryptography/utils.js';
 
 export const main = async () => {
   try {
+    const { bucketName, objectName, privateKey, rpc, contracts, storageAddress, spAddress, virtualGroupAddress } = await fs.readJSON('../cfg.json');
 
-    const { bucketName, objectName, privateKey, rpc, contracts, storageAddress, virtualGroupAddress } =
-      await fs.readJSON('../cfg.json');
+    const { abi: storageAbi } = await fs.readJSON(path.join(contracts, 'storage/IStorage.sol/IStorage.json'));
+    const { abi: virtualGroupAbi } = await fs.readJSON(path.join(contracts, 'virtualgroup/IVirtualGroup.sol/IVirtualGroup.json'));
+    const { abi: abiSp } = await fs.readJSON(path.join(contracts, 'storageprovider/IStorageProvider.sol/IStorageProvider.json'));
 
-    const { abi: storageAbi } = await fs.readJSON(
-      path.join(contracts, 'storage/IStorage.sol/IStorage.json')
-    );
-    const { abi: virtualGroupAbi } = await fs.readJSON(
-      path.join(contracts, 'virtualgroup/IVirtualGroup.sol/IVirtualGroup.json')
-    );
     const provider = new ethers.JsonRpcProvider(rpc);
+    const virtualGroup = new ethers.Contract(virtualGroupAddress, virtualGroupAbi, provider);
     const storage = new ethers.Contract(storageAddress, storageAbi, provider);
+    const sp = new ethers.Contract(spAddress, abiSp, provider);
+
     const [bucketInfo, extraInfo] = await storage.headBucket(bucketName);
-    let globalVirtualGroupFamilyId = bucketInfo.globalVirtualGroupFamilyId;
-
-    const filePath = path.join('.', 'uploadObject', objectName);
-    const fileBuffer = fs.readFileSync(filePath);
-
-    const pageRequest = {
+    const gvgFamily = await virtualGroup.globalVirtualGroupFamily(bucketInfo.globalVirtualGroupFamilyId);
+    const [spList, _] = await sp.storageProviders({
       key: '0x00',
       offset: 0,
       limit: 100,
-      countTotal: false,
+      countTotal: true,
       reverse: false,
-    };
-    const virtualGroup = new ethers.Contract(virtualGroupAddress, virtualGroupAbi, provider);
-    const [gvgFamilies, pageResponse] = await virtualGroup.globalVirtualGroupFamilies(pageRequest);
-    let spid = 0;
-    for (const gvgFamily of gvgFamilies) {
-      if (gvgFamily.id === globalVirtualGroupFamilyId) {
-        spid = gvgFamily.primarySpId;
-        break;
-      }
-    }
+    });
+    const spEndpoint = spList.filter((sp) => sp.id === gvgFamily.primarySpId)[0].endpoint;
+    const parsedUrl = new URL(spEndpoint);
+    const spHost = parsedUrl.hostname;
+    console.log('spEndpoint, spHost', spEndpoint, spHost);
+
     // input params
+    const filePath = path.join('.', 'uploadObject', objectName);
+    const fileBuffer = fs.readFileSync(filePath);
     const payloadSize = fileBuffer.length;
     const contentType = 'application/octet-stream';
     const currentDate = new Date().toISOString();
     const expiryTimestamp = new Date(Date.now() + 1000 * 1000).toISOString();
-    const sphosts = ['154.48.244.45:9033', '154.48.244.45:9034', '154.48.244.45:9035', '154.48.244.45:9036', '154.48.244.45:9037', '154.48.244.45:9038', '154.48.244.45:9039', '154.48.244.45:9040'];
-    let sphost = sphosts[spid - BigInt(1)];
-    console.log('sphost', sphost);
+
     const canonicalRequest = [
       'PUT',
       '/' + bucketName + '/' + objectName,
@@ -58,13 +49,11 @@ export const main = async () => {
       'x-gnfd-content-sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
       'x-gnfd-date:' + currentDate,
       'x-gnfd-expiry-timestamp:' + expiryTimestamp,
-      sphost,
+      spHost,
       '',
       'content-type;x-gnfd-content-sha256;x-gnfd-date;x-gnfd-expiry-timestamp',
     ].join('\n');
-    console.log(
-      `bucketName = ${bucketName}, objectName = ${objectName}, data = ${fileBuffer.toString()}`,
-    );
+    console.log(`bucketName = ${bucketName}, objectName = ${objectName}, data = ${fileBuffer.toString()}`);
     // console.log(canonicalRequest);
 
     const canonicalRequestBytes = utf8ToBytes(canonicalRequest);
@@ -78,25 +67,25 @@ export const main = async () => {
     const authorization = `GNFD1-ECDSA, Signature=${sig.slice(2)}`;
 
     const options = {
-      url: 'http://' + sphost + '/' + bucketName + '/' + objectName,
+      url: spEndpoint + '/' + bucketName + '/' + objectName,
       method: 'PUT',
       path: '/' + bucketName + '/' + objectName,
       httpVersion: 1.1,
       headers: {
-        'HOST': sphost,
+        HOST: spHost,
         'User-Agent': 'Greenfield (linux; amd64) greenfield-go-sdk/v0.1.0',
         'Content-Length': payloadSize,
-        'Authorization': authorization,
+        Authorization: authorization,
         'Content-Type': contentType,
         'X-Gnfd-Content-Sha256': 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
         'X-Gnfd-Date': currentDate,
         'X-Gnfd-Expiry-Timestamp': expiryTimestamp,
-        'Accept-Encoding': 'gzip'
+        'Accept-Encoding': 'gzip',
       },
-      body: fileBuffer
+      body: fileBuffer,
     };
 
-    fetch('http://' + sphost + '/' + bucketName + '/' + objectName, options)
+    fetch(spEndpoint + '/' + bucketName + '/' + objectName, options)
       .then((response) => {
         if (response.ok) {
           console.log('File data sent successfully!');
